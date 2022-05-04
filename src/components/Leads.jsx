9d057704d078b629f338a4fca21f09ca39f2c839
Alessandro Hugen <alessandrohugen@gmail.com>
2022-05-04 20:40:13 -0300

    feat: create category select

--- src/components/Leads.jsx
+++ src/components/Leads.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react'
 import getAPI from '../services/index'
-import { Form, Table } from 'react-bootstrap'
+import { Table } from 'react-bootstrap'
+import { Dropdown } from 'react-bootstrap'
 
 export function Leads(data) {
   const [leadName, setLeadName] = useState([])
   const [leadData, setLeadData] = useState([])
+  const [category, setCategory] = useState([])
 
   async function getData() {
     const data = await getAPI()
@@ -17,29 +19,38 @@ export function Leads(data) {
 
   return (
     <>
+      <h2 className="text-center p-2 fw-light fst-italic">Encontre Leads</h2>
       <input
         type="text"
         placeholder="Busque pelo nome do contato"
-        className="form-control p-2 m-2 w-50"
+        className="p-2 m-2 w-50 rounded text-center"
         onChange={({ target }) => setLeadName(target.value)}
       />
-      <Form>
-        {leadData &&
-          leadData.map(lead => (
-            <Form.Check
-              type="checkbox"
-              className="mb-3 text-light"
-              label={lead.company.bs}
-              value={lead.company.bs}
-            />
-          ))}
-      </Form>
+      <form>
+        <label htmlFor="category" className="text-center p-2 fst-italic">
+          Encontra a empresa por categoria:
+          <select
+            name="category"
+            id="category"
+            onChange={({ target }) => setCategory(target.value)}
+            className="form-select form-select-sm m-3"
+          >
+            {leadData &&
+              leadData.map(lead => (
+                <option key={lead} value={lead.company.bs}>
+                  {lead.company.bs}
+                </option>
+              ))}
+          </select>
+        </label>
+      </form>
       <Table striped bordered hover variant="dark" className="m-2 w-75">
         <thead>
           <tr>
             <th>Nome</th>
             <th>Empresa</th>
             <th>E-mail</th>
+            <th>Telefone</th>
           </tr>
         </thead>
         <tbody>
@@ -51,6 +62,7 @@ export function Leads(data) {
                   <td>{lead.name}</td>
                   <td>{lead.company.name}</td>
                   <td>{lead.email}</td>
+                  <td>{lead.phone}</td>
                 </tr>
               ))}
         </tbody>
